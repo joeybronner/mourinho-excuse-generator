52c6525dab99db6249e2f69f6292b28d6072e435
Joey Bronner <joeybronner@gmail.com>
2015-10-28 22:42:19 +0100

    Remove splash screen

--- app/src/main/java/fr/joeybronner/mourinhoescusegenerator/MainActivity.java
+++ app/src/main/java/fr/joeybronner/mourinhoescusegenerator/MainActivity.java
@@ -40,13 +40,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            openView(getBaseContext(), GeneratedExcuse.class);
             return true;
         }
 
